f7ac8b8db55d96426d9fb86c4b9fb5efb5e49499
Kyle Tierney <ktierney@brainshark.com>
2019-09-25 14:37:26 -0400

    Revert "IOS 13 corrupted image fix"
    This reverts commit 358290cbfcb24910047e86e55278eaf4ba71bed9.

--- src/ios/CDVCamera.m
+++ src/ios/CDVCamera.m
@@ -671,21 +671,17 @@ static NSString* toBase64(NSData* data) {
 {
     CDVPictureOptions* options = self.pickerController.pictureOptions;
     CDVPluginResult* result = nil;
-	
-	NSMutableData *imageDataWithExif = [NSMutableData data];
 
     if (self.metadata) {
         CGImageSourceRef sourceImage = CGImageSourceCreateWithData((__bridge CFDataRef)self.data, NULL);
         CFStringRef sourceType = CGImageSourceGetType(sourceImage);
 
-        CGImageDestinationRef destinationImage = CGImageDestinationCreateWithData((__bridge CFMutableDataRef)imageDataWithExif, sourceType, 1, NULL);
+        CGImageDestinationRef destinationImage = CGImageDestinationCreateWithData((__bridge CFMutableDataRef)self.data, sourceType, 1, NULL);
         CGImageDestinationAddImageFromSource(destinationImage, sourceImage, 0, (__bridge CFDictionaryRef)self.metadata);
         CGImageDestinationFinalize(destinationImage);
 
         CFRelease(sourceImage);
         CFRelease(destinationImage);
-    } else {
-        imageDataWithExif = [self.data mutableCopy];
     }
 
     switch (options.destinationType) {
@@ -696,7 +692,7 @@ static NSString* toBase64(NSData* data) {
             NSString* filePath = [self tempFilePath:extension];
 
             // save file
-            if (![imageDataWithExif writeToFile:filePath options:NSAtomicWrite error:&err]) {
+            if (![self.data writeToFile:filePath options:NSAtomicWrite error:&err]) {
                 result = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION messageAsString:[err localizedDescription]];
             }
             else {
@@ -706,7 +702,7 @@ static NSString* toBase64(NSData* data) {
             break;
         case DestinationTypeDataUrl:
         {
-            result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:toBase64(imageDataWithExif)];
+            result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:toBase64(self.data)];
         }
             break;
         case DestinationTypeNativeUri:
@@ -722,8 +718,6 @@ static NSString* toBase64(NSData* data) {
     self.pickerController = nil;
     self.data = nil;
     self.metadata = nil;
-	
-	imageDataWithExif = nil;
 
     if (options.saveToPhotoAlbum) {
         ALAssetsLibrary *library = [ALAssetsLibrary new];
